67d30bb9bba6814ed0bc413170173bf5693d930e
Jasurbek Shonazarov <jasurbekshonazarov482@gmail.com>
2023-01-06 16:03:30 +0500

    projectni qushdim

---
+++ Boshliq.java
@@ -0,0 +1,19 @@
+package onlineShop;
+
+public class Boshliq {
+    String name;
+    String surname;
+    int age;
+    int year;
+
+    public Boshliq(String ism, String familiya, int yosh, int yili){
+        name=ism;
+        surname=familiya;
+        age=yosh;
+        year=yili;
+    }
+    public Boshliq(){}
+
+
+
+}
